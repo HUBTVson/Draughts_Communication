7c404fefd0c3f0e2514a7911163215489fc5408f
Jakub Kondracki <s188767@student.pg.edu.pl>
2024-06-04 19:22:44 +0200

    Fixing server.py

--- client.py
+++ client.py
@@ -2,7 +2,6 @@ import socket
 import threading
 import json
 from colorama import Fore, Style
-
 from piece import Piece
 from square import Square
 
@@ -16,52 +15,36 @@ class CheckersClient:
         self.player_id = None
 
     def listen_for_updates(self):
-        # Receive player id
         self.player_id = self.server.recv(1024).decode()
-        print(f"You are player{self.player_id}")
+        print(f"You are player {self.player_id}")
 
         while True:
-            # Receive message from server
             message = self.server.recv(1024).decode()
             if message:
-                # Decode message
                 data = json.loads(message)
-
-                # Check for message type
                 if data["status"] == "EXIT":
-                    # Close client connection and exit
                     print(data["message"])
                     self.server.close()
                     exit()
-
                 elif data["status"] == "Invalid move":
-                    # Print error message and prompt user for input
                     print("Invalid move. Try again.")
                     self.get_user_input()
-
                 elif data["status"] == "update":
-                    # Update game state and render board
                     self.game_state = json.loads(data["game_state"])
                     self.render_board()
                     if self.game_state["turn"] == self.player_id:
                         self.get_user_input()
-
                 else:
-                    # Unknown message
                     print("Unknown message")
                     print(data)
 
     def send_move(self, start, end, captures=[]):
-        # Send move to server
-
         move = {
             "from_x": start[0],
             "from_y": start[1],
             "to_x": end[0],
             "to_y": end[1]
         }
-
-        # Serialize move and send to server
         msg = json.dumps({
             "status": "move",
             "move": move
@@ -69,9 +52,6 @@ class CheckersClient:
         self.server.send(msg)
 
     def render_board(self):
-        # Render game board
-
-        # ADD PRETTY PRINTING
         board = self.game_state["board"]
         new_board = []
         for row in range(8):
@@ -114,32 +94,26 @@ class CheckersClient:
         print(draw_b)
 
     def get_user_input(self):
-        # Get user input for move
         try:
-            print(f"Player{self.player_id}'s turn")
+            print(f"Player {self.player_id}'s turn")
+            user_input = input("Enter move (start_x,start_y end_x,end_y) or 'exit' to quit: ").strip()
+            if user_input.lower() == 'exit':
+                self.shutdown()
+            else:
+                start, end = user_input.split()
+                start = tuple(map(int, start.split(',')))
+                end = tuple(map(int, end.split(',')))
+                self.send_move(start, end)
+        except Exception as e:
+            print(f"Invalid Input, try again")
+            self.get_user_input()
 
-            # CHANGE HERE START
-            start = input("Enter start position (row,col): ").split(',')
-            end = input("Enter end position (row,col): ").split(',')
-            # CHANGE HERE END
-        except:
-            self.shutdown(None, None)
-
-        # Send move to server
-        start = (int(start[0]), int(start[1]))
-        end = (int(end[0]), int(end[1]))
-        self.send_move(start, end)
-
-    def shutdown(self, signum, frame):
-        # Send exit message to server
-        self.server.send(json.dumps({
-            "status": "EXIT"
-        }).encode())
-        # Shutdown client
-        print("\nShutting down client...")
+    def shutdown(self):
+        self.server.send(json.dumps({"status": "EXIT"}).encode())
+        print("Shutting down client...")
         self.server.close()
         exit()
 
 
 if __name__ == "__main__":
-    server = CheckersClient()
+    client = CheckersClient()
--- server.py
+++ server.py
@@ -7,14 +7,13 @@ from typing import Tuple, List
 
 
 class CheckersServer:
-    def __init__(self, host='localhost', port=5555) -> None:
+    def __init__(self, host='localhost', port=5555):
         self.host = host
         self.port = port
         self.start_server(host, port)
         self.initialize_game()
 
     def start_server(self, host: str, port: int) -> None:
-        # Start server on specified host and port
         self.clients: List[Tuple[socket.socket, int]] = []
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((host, port))
@@ -24,31 +23,22 @@ class CheckersServer:
         self.lock = threading.Lock()
 
     def initialize_game(self) -> None:
-        # Initialize game state
         self.game = Checkers()
 
     def broadcast_message(self, message: str) -> None:
-        # Broadcast message to all clients
         for client, _ in self.clients:
             client.send(message.encode())
 
     def restart_server(self) -> None:
-        # Broadcast message to clients
         msg = json.dumps({
             "status": "EXIT",
             "message": "Server is restarting"
         })
         self.broadcast_message(msg)
-
-        # Close all client connections
         for client, _ in self.clients:
             client.close()
-
-        # Clear list of clients and close server
         self.clients = []
         self.server.close()
-
-        # Restart server
         self.initialize_game()
         self.start_server(self.host, self.port)
         self.start()
@@ -56,16 +46,10 @@ class CheckersServer:
     def handle_client(self, client_socket: socket.socket, player_id: int, debug: bool = True) -> None:
         while True:
             try:
-                # Receive message from client
                 message = client_socket.recv(1024).decode()
-                # Deserialize message
                 message = json.loads(message)
-
-                # Check for message type
                 if message["status"] == "EXIT":
                     raise Exception("Client exited")
-
-                # Process move
                 if message["status"] == "move":
                     message = message["move"]
                     with self.lock:
@@ -74,15 +58,11 @@ class CheckersServer:
             except Exception as e:
                 if debug:
                     print(e)
-
-                # Remove client from list of clients
                 try:
                     self.clients.remove((client_socket, player_id))
                     client_socket.close()
                 except Exception as ignored:
                     pass
-
-                # Broadcast message and exit thread
                 self.broadcast_message(json.dumps({
                     "status": "EXIT",
                     "message": f"Player {player_id} has left the game"
@@ -90,13 +70,9 @@ class CheckersServer:
                 break
 
     def process_move(self, client_socket: socket.socket, move) -> None:
-        # Process move from client
-
-        # Validate move
         if self.game.move(move):
             winner = self.game.winner
             if winner is not None:
-                # If game has ended, broadcast message to clients
                 msg = json.dumps({
                     "status": "EXIT",
                     "message": f"Player {winner} has won the game"
@@ -104,18 +80,12 @@ class CheckersServer:
                 self.broadcast_message(msg)
                 self.restart_server()
             else:
-                # Broadcast game state to all clients
                 self.broadcast_game_state()
         else:
-            # If move is invalid, send message to client
             client_socket.send(json.dumps({"status": "Invalid move"}).encode())
 
     def broadcast_game_state(self) -> None:
-        # Broadcast game state to all clients
-
-        # Serialize game state
         game_state = json.dumps(self.game.state)
-        # Send game state to all clients
         for client, _ in self.clients:
             msg = json.dumps({
                 "status": "update",
@@ -128,35 +98,24 @@ class CheckersServer:
         workers = []
 
         while len(self.clients) < 2:
-            # Accept connection from client
             client_socket, addr = self.server.accept()
-
-            # Assign player id to client
             player_id = np.random.choice(player_ids)
             player_ids.remove(player_id)
-
-            # Add client to list of clients
             self.clients.append((client_socket, player_id))
             print(f"Player {player_id} connected from {addr}")
-
-            # Start a new thread to handle client
-            thread = threading.Thread(target=self.handle_client, args=(
-                client_socket, player_id))
+            thread = threading.Thread(target=self.handle_client, args=(client_socket, player_id))
             workers.append(thread)
             thread.start()
-
-            # Send player id to client
             client_socket.send(str(player_id).encode())
 
         self.broadcast_game_state()
 
-        # Restart server when game ends
         for worker in workers:
             worker.join()
         self.restart_server()
 
     def shutdown(self, signum, frame):
-        print("\nShutting down server...")
+        print("Shutting down server...")
         for client, _ in self.clients:
             client.close()
         self.server.close()
